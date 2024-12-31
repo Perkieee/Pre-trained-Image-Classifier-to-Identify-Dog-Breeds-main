4e7c7ef17ede7620c8d39936eb12c2f4a953fa1a
Kevin Walter <144652422+Perkieee@users.noreply.github.com>
2024-12-31 15:59:42 +0300

    Update run_models_batch_uploaded (1).sh

--- run_models_batch_uploaded (1).sh
+++ run_models_batch_uploaded (1).sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # */AIPND-revision/intropyproject-classify-pet-images/run_models_batch_uploaded.sh
 #                                                                             
-# PROGRAMMER: GLORIOUS MUSYOKA.
+# PROGRAMMER: KEVIN WALTER
 # DATE CREATED: 01/11/2024                                  
 # REVISED DATE: 10/11/2024 - 
 # PURPOSE: Runs all three models to test which provides 'best' solution on the Uploaded Images.
